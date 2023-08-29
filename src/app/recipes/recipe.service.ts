6abe425c5f8c0df8b855f71d9fb5b950f9186ff1
Rohit Vishwakarma <rohitvishwakarma1819@gmail.com>
2023-08-29 11:21:32 +0530

    added feature to add recipe ingredients to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,8 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe-list/recipe.mode';
 import { Ingredient } from '../shared/ingredients.model';
-
+import { ShoppingListService } from '../shopping-list/shpping-list.service';
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
@@ -27,7 +28,13 @@ export class RecipeService {
     ),
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
